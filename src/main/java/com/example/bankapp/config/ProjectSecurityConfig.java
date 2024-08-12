ac12ee034a1e558b88a977f63f98d011e49c93ca
steparon <kateryna.stepanova99@gmail.com>
2024-08-12 13:33:53 +0300

    Added Data Base

--- src/main/java/com/example/bankapp/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/bankapp/config/ProjectSecurityConfig.java
@@ -1,17 +1,17 @@
-package com.example.bankapp;
+package com.example.bankapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -28,16 +28,11 @@ public class ProjectSecurityConfig {
         http.httpBasic(withDefaults());
 
         return http.build();
-}
+    }
 
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user")
-                .password("{bcrypt}$2a$12$yAOZ2LOjV8Eb6p.cJy98ue.pX6xWa1dhw8hTF3AUHpDDdxxguUraC")
-                .authorities("read").build();
-        UserDetails admin = User.withUsername("admin")
-                .password("{bcrypt}$2a$12$8LW4bezOausHopKSRmI0UOtIlxKJGpeYcqzOO.ipG/BT24Ht6jRju")
-                .authorities("admin").build();
-        return new InMemoryUserDetailsManager(user, admin);
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
